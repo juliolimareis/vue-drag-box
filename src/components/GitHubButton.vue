23d67187a57fdc5ac01c987070bc398bbbb9656b
Júlio César Lima Reis <juliolimareis@gmail.com>
2024-03-15 19:08:31 -0300

    Fix link github

--- src/components/GitHubButton.vue
+++ src/components/GitHubButton.vue
@@ -1,7 +1,7 @@
 <template>
   <a
     target="_blank"
-    href="https://github.com/juliolimareis/ygo-find-cards"
+    href="https://github.com/juliolimareis/vue-drag-box"
     type="button"
     class="text-white bg-[#24292F] hover:bg-[#24292F]/90 focus:ring-4 focus:outline-none focus:ring-[#24292F]/50 font-medium rounded-lg text-sm px-5 py-2 text-center inline-flex items-center dark:focus:ring-gray-500 dark:hover:bg-[#050708]/80"
   >
